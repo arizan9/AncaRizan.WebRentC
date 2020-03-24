c89b7f5a2c90d7d31a5b36145955d396e3a5a24e
arizan9 <arizan9@users.noreply.github.com>
2020-03-24 22:42:56 +0200

    Initial Solution Setup

---
+++ AncaRizan.WebRentC/AncaRizan.WebRentC.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AncaRizan.WebRentC.WebUI.MvcApplication" Language="C#" %>
---
+++ AncaRizan.WebRentC/AncaRizan.WebRentC.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AncaRizan.WebRentC.WebUI.Startup))]
+namespace AncaRizan.WebRentC.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
